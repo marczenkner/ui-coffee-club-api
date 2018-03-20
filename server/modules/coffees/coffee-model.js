d209fc522d7f2defd39dcc395781319d3b729bae
Marco Zenkner <marco.zenkner@taillight.com>
2018-03-20 10:16:22 -0500

    Added basic request validation for addCoffees

--- server/modules/coffees/coffee-model.js
+++ server/modules/coffees/coffee-model.js
@@ -4,12 +4,26 @@ const CoffeeSchema = new Schema({
     title: {
         type: String,
         required: true,
-        minLength: [5, '5 characters long at least'],
+        minLength: [5, '5 characters long at least']
     },
     description: {
         type: String,
         required: true,
-        minLength: [10, '10 characters long at least'],
+        minLength: [10, '10 characters long at least']
+    },
+    boughtLocation: {
+        type: String,
+        required: true,
+        minLength: [1, '10 characters long at least']
+    },
+    flavor: {
+        type: Array,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 10
     }
 }, { timestamps: true });
 
--- server/modules/coffees/controller.js
+++ server/modules/coffees/controller.js
@@ -1,19 +1,74 @@
 import Coffee from './coffee-model';
 
 export const createCoffee = async (req, res) => {
-    const { title, description } = req.body;
-    console.log(title, description);
-    const newCoffee = new Coffee({ title, description });
+    const { title, description, boughtLocation, flavor, rating } = req.body;
+
+        if (!title) {
+            return res.status(400).json({ error: true, message: 'Title must be provided!' });
+        } else if (typeof title !== 'string') {
+            return res.status(400).json({ error: true, message: 'Title must be a string!' });
+        } else if (title.length < 5) {
+            return res.status(400).json({ error: true, message: 'Title must be at least 5 characters long!' });
+        }
+
+        if (!description) {
+            return res.status(400).json({ error: true, message: 'Description must be provided!' });
+        } else if (typeof description !== 'string') {
+            return res.status(400).json({ error: true, message: 'Description must be a string!' });
+        } else if (description.length < 5) {
+            return res.status(400).json({ error: true, message: 'Description must be at least 10 characters long!' });
+        }
+
+        if (!boughtLocation) {
+            return res.status(400).json({ error: true, message: 'Where bought must be provided!' });
+        } else if (typeof boughtLocation !== 'string') {
+            return res.status(400).json({ error: true, message: 'Where bought must be a string!' });
+        } else if (boughtLocation.length < 3) {
+            return res.status(400).json({ error: true, message: 'Where bought must be at least 3 characters long!' });
+        }
+
+        if (!flavor) {
+            return res.status(400).json({ error: true, message: 'Flavor must be provided!' });
+        }
+
+        // else if (typeof flavor !== 'array') {
+        //     return res.status(400).json({ error: true, message: 'Flavor must be an array!' });
+        // }
+
+        else if (flavor.length < 1) {
+            return res.status(400).json({ error: true, message: 'Please define at least one flavor.' });
+        }
+
+        if (!rating) {
+            return res.status(400).json({ error: true, message: 'Rating must be provided!' });
+        }
+
+        // else if (typeof rating != 'number') {
+        //     return res.status(400).json({ error: true, message: 'Rating must be a number!' });
+        // }
+
+        else if (rating > 10 || rating < 0) {
+            return res.status(400).json({ error: true, message: 'Rating must be a number between 0 and 10' });
+        }
+
+    const newCoffee = new Coffee({ title, description, boughtLocation, flavor, rating });
 
     try {
         return res.status(201).json({ coffee: await newCoffee.save() });
     } catch(e) {
         return res.status(e.status).json({ error: true, message: 'Error creating new Coffee.' });
     }
-};
+}
 
 export const getCoffees = async (req, res) => {
-    console.log('get coffees called');
+    try {
+        return res.status(200).json({ coffees: await Coffee.find({})});
+    } catch(error) {
+        return res.status(error.status).json({ error: true, message: 'Error getting Coffees' });
+    }
+}
+
+export const getCoffeeByGroup = async (req, res) => {
     try {
         return res.status(200).json({ coffees: await Coffee.find({_id: "5a96c4924288d1045dd75b2f"}) });
     } catch(error) {
