ac654570308508f93913d7a54151e76178d2c98c
Marco Zenkner <marco.zenkner@taillight.com>
2018-03-21 07:30:49 -0500

    Added /upload functionality for images

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import dbConfig from './config/dbConfig';
 import middleWareConfig from './config/middleware';
 import { CoffeeRoutes } from './modules/coffees';
+// Imports for image-uploader
+import path from 'path';
+import formidable from 'formidable';
+import fs from 'fs';
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +17,38 @@ dbConfig();
 middleWareConfig(app);
 
 app.use('/api', CoffeeRoutes);
+
+app.post('/upload', function(req, res){
+
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+
+});
 
 app.listen(PORT, (err) => {
   if (err) {
---
+++ server/modules/upload-images/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+// routes.post('/add-image', CoffeeController.createCoffee);
+
+export default routes;
+
