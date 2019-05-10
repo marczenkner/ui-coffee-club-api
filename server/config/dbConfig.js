a5536bb9ac1b88ec629d7f2b3d223fc35118bb80
Marco Zenkner <marco.zenkner@taillight.com>
2019-05-10 12:48:39 -0500

    First Commit

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 
 export default () => {
-
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://heroku_czqz7rps:ff81c6jlj0tbbj9uvpngpooset@ds247178.mlab.com:47178/heroku_czqz7rps');
-    mongoose.connection
-        .once('open', () => console.log('Mongodb Running'))
-        .on('error', err => console.error(err));
+  mongoose.Promise = global.Promise;
+  mongoose.connect('mongodb://heroku_czqz7rps:ff81c6jlj0tbbj9uvpngpooset@ds247178.mlab.com:47178/heroku_czqz7rps');
+  mongoose.connection
+    .once('open', () => console.log('Mongodb Running'))
+    .on('error', err => console.error(err));
 };
 
--- server/modules/coffees/coffee-model.js
+++ server/modules/coffees/coffee-model.js
@@ -1,30 +1,30 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const CoffeeSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-        minLength: [5, '5 characters long at least']
-    },
-    description: {
-        type: String,
-        required: true,
-        minLength: [10, '10 characters long at least']
-    },
-    boughtLocation: {
-        type: String,
-        required: true,
-        minLength: [1, '10 characters long at least']
-    },
-    flavor: {
-        type: Array,
-        required: true
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 10
-    }
+  title: {
+    type: String,
+    required: true,
+    minLength: [5, '5 characters long at least'],
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: [10, '10 characters long at least'],
+  },
+  boughtLocation: {
+    type: String,
+    required: true,
+    minLength: [1, '10 characters long at least'],
+  },
+  flavor: {
+    type: Array,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+  },
 }, { timestamps: true });
 
 export default mongoose.model('Coffee', CoffeeSchema);
