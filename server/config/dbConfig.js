ab847f8b3b86d09d15456b3d5703add61faa15c5
Marco Zenkner <marco.zenkner@taillight.com>
2019-05-14 10:08:40 -0500

    Created createQuestion method

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default () => {
   mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://heroku_czqz7rps:ff81c6jlj0tbbj9uvpngpooset@ds247178.mlab.com:47178/heroku_czqz7rps');
+  mongoose.connect('mongodb://insights_api_one:Babydol1s@ds155616.mlab.com:55616/heroku_qmtx7qw7');
   mongoose.connection
     .once('open', () => console.log('Mongodb Running'))
     .on('error', err => console.error(err));
--- server/modules/questions/controller.js
+++ server/modules/questions/controller.js
@@ -13,64 +13,31 @@ export const editQuestion = async (req, res) => {
 };
 
 export const createQuestion = async (req, res) => {
-    console.log('createQuestion called with ', req, res);
-  //   const { title, description, boughtLocation, flavor, rating } = req.body;
-  //
-  // if (!title) {
-  //   return res.status(400).json({ error: true, message: 'Title must be provided!' });
-  // } else if (typeof title !== 'string') {
-  //   return res.status(400).json({ error: true, message: 'Title must be a string!' });
-  // } else if (title.length < 5) {
-  //   return res.status(400).json({ error: true, message: 'Title must be at least 5 characters long!' });
-  // }
-  //
-  // if (!description) {
-  //   return res.status(400).json({ error: true, message: 'Description must be provided!' });
-  // } else if (typeof description !== 'string') {
-  //   return res.status(400).json({ error: true, message: 'Description must be a string!' });
-  // } else if (description.length < 5) {
-  //   return res.status(400).json({ error: true, message: 'Description must be at least 10 characters long!' });
-  // }
-  //
-  // if (!boughtLocation) {
-  //   return res.status(400).json({ error: true, message: 'Where bought must be provided!' });
-  // } else if (typeof boughtLocation !== 'string') {
-  //   return res.status(400).json({ error: true, message: 'Where bought must be a string!' });
-  // } else if (boughtLocation.length < 3) {
-  //   return res.status(400).json({ error: true, message: 'Where bought must be at least 3 characters long!' });
-  // }
-  //
-  // if (!flavor) {
-  //   return res.status(400).json({ error: true, message: 'Flavor must be provided!' });
-  // }
-  //
-  // // else if (typeof flavor !== 'array') {
-  // //     return res.status(400).json({ error: true, message: 'Flavor must be an array!' });
-  // // }
-  //
-  // else if (flavor.length < 1) {
-  //   return res.status(400).json({ error: true, message: 'Please define at least one flavor.' });
-  // }
-  //
-  // if (!rating) {
-  //   return res.status(400).json({ error: true, message: 'Rating must be provided!' });
-  // }
-  //
-  // // else if (typeof rating != 'number') {
-  // //     return res.status(400).json({ error: true, message: 'Rating must be a number!' });
-  // // }
-  //
-  // else if (rating > 10 || rating < 0) {
-  //   return res.status(400).json({ error: true, message: 'Rating must be a number between 0 and 10' });
-  // }
-  //
-  // const newCoffee = new Question({ title, description, boughtLocation, flavor, rating });
-  //
-  // try {
-  //   return res.status(201).json({ coffee: await newCoffee.save() });
-  // } catch (e) {
-  //   return res.status(e.status).json({ error: true, message: 'Error creating new Coffee.' });
-  // }
+    const { questionText, questionOrder, questionWeight, questionHasInput, questionInputType, questionInputText, questionIsDependant, questionIsDependantOn, questionCustomBackground, questionStyles, questionIsVisible } = req.body;
+
+    if (!questionText) {
+        return res.status(400).json({ error: true, message: 'Question Text must be provided!' });
+    }
+
+    if (!questionWeight) {
+        return res.status(400).json({ error: true, message: 'Question Weight must be provided!' });
+    }
+
+    if (!questionInputType) {
+        return res.status(400).json({ error: true, message: 'Question Input Type must be provided!' });
+    }
+
+    if (!questionIsVisible) {
+        return res.status(400).json({ error: true, message: 'Question IsVisible must be provided!' });
+    }
+
+    const newQuestion = new Question({ questionText, questionOrder, questionWeight, questionHasInput, questionInputType, questionInputText, questionIsDependant, questionIsDependantOn, questionCustomBackground, questionStyles, questionIsVisible });
+
+  try {
+    return res.status(201).json({ await newQuestion.save());
+  } catch (e) {
+    return res.status(e.status).json({ error: true, message: 'Error creating new Question.' });
+  }
 };
 
 export const deleteQuestion = async (req, res) => {
